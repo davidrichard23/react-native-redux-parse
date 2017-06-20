cb1ccce2f28ad60951b45d5a7e7f74944b0e41db
davidRichard23 <d2391@me.com>
2017-06-19 20:28:08 -0400

    added animation

--- src/components/SegmentedSelector.js
+++ src/components/SegmentedSelector.js
@@ -1,27 +1,36 @@
 import React, {Component} from 'react'
-import { View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, LayoutAnimation} from 'react-native'
 import colors from '../utils/colors.json'
 
 
 
 export default class SegmentedSelector extends Component {
 
+	componentWillUpdate() {
+		LayoutAnimation.easeInEaseOut()
+	}
+
 	render() {
 
 		const choices = this.props.choices
 		const onChange = this.props.onChange
 		const selectedIndex = this.props.selectedIndex
+		const flexValues = [selectedIndex === 0 ? 0 : 1, 1, selectedIndex === 1 ? 0 : 1]
 
 		return (
 			<View style={styles.container}>
+				<View style={styles.underlay}>
+					<View style={{flex: flexValues[0], backgroundColor: 'transparent', borderRadius: 4}} />
+					<View style={{flex: flexValues[1], backgroundColor: colors.darkest, borderRadius: 4}} />
+					<View style={{flex: flexValues[2], backgroundColor: 'transparent', borderRadius: 4}} />
+				</View>
 				{choices.map((item, index) => {
-					const bgColor = selectedIndex === index ? colors.primary : 'transparent'
-					const inlineStyle = {backgroundColor: bgColor}
+					const color = selectedIndex === index ? colors.primary : 'rgba(255,255,255,0.7)'
 
 					return(
-						<View key={item} style={[styles.buttonContainer, inlineStyle]}>
+						<View key={item} style={[styles.buttonContainer]}>
 							<TouchableOpacity style={styles.button} onPress={() => onChange(index)}>
-								<Text style={styles.text}>{item}</Text>
+								<Text style={[styles.text, {color: color}]}>{item}</Text>
 							</TouchableOpacity>
 						</View>
 					)
@@ -44,12 +53,21 @@ const styles = StyleSheet.create({
 		flex: 1,
 		borderRadius: 4,
 	},
+	underlay: {
+		position: 'absolute',
+		flexDirection: 'row',
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+	},
+	underlaySelected: {
+
+	},
 	button: {
-		// flex: 1,
 		paddingVertical: 10,
 	},
 	text: {
-		color: 'white',
 		textAlign: 'center',
 	},
 })
